8e64c535c6c1f7acfcaf3079705614a0fc15b2d5
Felipe Borges <bumbleblo2013@gmail.com>
2021-04-10 12:41:16 -0300

    Initial quadtree
    Signed-off-by: Felipe Borges <bumbleblo2013@gmail.com>

--- 2DCollisionDetection/inc/CollisionDetection.hpp
+++ 2DCollisionDetection/inc/CollisionDetection.hpp
@@ -6,6 +6,50 @@
 #include <cstdint>
 #include <vector>
 
+class Rectangle{
+    private:
+    public:
+}
+
+class QuadTree{
+    private:
+        int MAX_OBJECTS = 40;
+        int MAX_LEVELS = 3;
+        int level;
+        vector<Ball *> balls;
+        Rectangle bounds;
+
+        QuadTree nodes[4];
+
+    void clear(){
+        balls.clear();
+        for(int i = 0; i < 4; ++i){
+            if(nodes[i] != NULL){
+                nodes[i].clear();
+                nodes[i] = NULL;
+            }
+        }
+    }
+
+    void split(){
+        int subWith = (int)bounds.getWidth()/2;
+        int subHeight = (int)bounds.getHeight()/2;
+
+        int x = (int)bounds.getX();
+        int y = (int)bounds.getY();
+
+        nodes[0] = new QuadTree(level + 1, new Rectangle(
+    }
+
+    public:
+
+        QuadTree(int plevel, Rectangle pbounds){
+            level = plevel;
+            bounds = pbounds;
+        }
+
+}
+
 namespace ColDetect
 {
     void EfficientCollisionDetection(std::vector<Ball *> balls, int32_t table_width, int32_t table_height)
